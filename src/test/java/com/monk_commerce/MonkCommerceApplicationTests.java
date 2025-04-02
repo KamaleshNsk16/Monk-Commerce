c7dd62806ab5457ebc3d0bc8230b4bfb4ec7c82e
kamaleshsgp <kamaleshsgp@gmail.com>
2025-04-02 18:37:16 +0530

    dependencies added

---
+++ src/test/java/com/monk_commerce/MonkCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monk_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
